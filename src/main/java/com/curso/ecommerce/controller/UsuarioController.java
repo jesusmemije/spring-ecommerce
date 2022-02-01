0891b2378eccbf0361ee8fbd0812cb9142c0b683
Jesús González <memije.dev@gmail.com>
2022-02-01 16:42:38 -0600

    Cerrar la sesión del usuario

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -96,5 +96,11 @@ public class UsuarioController {
 		model.addAttribute("sesion", session.getAttribute("idusuario"));
 		return "usuario/detallecompra";
 	}
+	
+	@GetMapping("/cerrar")
+	public String cerrarSesion ( HttpSession session ) {
+		session.removeAttribute("idusuario");
+		return "redirect:/";
+	}
 
 }
